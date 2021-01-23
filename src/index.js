0cbb85ea3e0a5f8ebecbb712bb5225003e573ec7
KosM <kosukemorimoto1@gmail.com>
2021-01-23 11:25:48 +0900

    modified index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./wdyr"; // not sure whether it's working
 
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
@@ -30,6 +31,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
